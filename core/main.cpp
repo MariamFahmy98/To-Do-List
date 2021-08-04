52317f3784a6aa74414e09608793cffed0260000
Mariam Fahmy <mariamfahmy66@gmail.com>
2021-08-04 21:50:38 +0200

    ToDoList, modyfing button component.

--- core/main.cpp
+++ core/main.cpp
@@ -1,20 +1,26 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
-#include <QtQuick/QQuickView>
-#include <QQmlContext>
 #include "modelInterface.h"
+
+#include <QtGui/qguiapplication.h>
+#include <QtQml/qqmlapplicationengine.h>
+#include <QtQml/qqmlcontext.h>
+#include <QtQuick/qquickview.h>
 
 int main(int argc, char*argv[])
 {
+    QQuickWindow::setGraphicsApi(QSGRendererInterface::OpenGLRhi);
+
     ModelInterface modelInterface;
+
     QGuiApplication app(argc, argv);
     QQuickView view;
     QQmlContext *context = view.engine()->rootContext();
 
     context->setContextProperty("modelInterface", &modelInterface);
 
-    view.setSource(QUrl("qrc:/main.qml"));
+    view.setSource(QUrl("qrc:/qml/main.qml"));
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
     view.setMinimumSize(QSize(500,800));
     view.show();
+
     return app.exec();
 }
